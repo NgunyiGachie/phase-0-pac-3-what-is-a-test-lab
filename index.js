2ceb68ad491a40f624ef5bb825e29fca1c23b8cd
NgunyiGachie <antogachie@gmail.com>
2024-03-18 14:36:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+let height = 10;
+if (height > 0 && height <40) {console.log("height is greater than 0 but less than 40")};
+
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
